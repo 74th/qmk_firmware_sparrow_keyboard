99617b8af61acc90654fa6efbe3ba8290232eb7e
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2024-01-06 16:01:13 +0900

    sparrow60c

---
+++ keyboards/sparrow60c/config.h
@@ -0,0 +1,11 @@
+// Copyright 2023 74th (@74th)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define PIMORONI_TRACKBALL_ADDRESS 0x0A
+#define I2C1_SCL_PIN GP1
+#define I2C1_SDA_PIN GP0
+#define I2C_DRIVER I2CD0
+#define F_SCL 100000
+#define PIMORONI_TRACKBALL_SCALE 1

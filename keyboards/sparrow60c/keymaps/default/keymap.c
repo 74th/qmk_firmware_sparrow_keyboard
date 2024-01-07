17343bc9828a54863bd9fd9f8ff4dee048f6cf9a
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2024-01-07 20:46:12 +0900

    add LED

--- keyboards/sparrow60c/keymaps/default/keymap.c
+++ keyboards/sparrow60c/keymaps/default/keymap.c
@@ -3,6 +3,13 @@
 
 #include QMK_KEYBOARD_H
 
+enum layer_number {
+    _BASE = 0,
+    _RAISE,
+    _LAYER_2,
+    _LAYER_3,
+};
+
 enum custom_keycodes {
     SCROLL = SAFE_RANGE,
 };
@@ -53,14 +60,73 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * ├────┼───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
      * │Shft│ Z │ X │ C │ V │ B │ ] │   │ N │ M │ , │ . │ / │Sft│
      * └────┴───┼───┼───┴┬──┴─┬─┴──┬┴───┼───┴┬──┴─┬─┴──┬┴──┬┴───┘
-     *          │SCR│LGUI│LALT│SPC │CLIK│Ent │RCTL│Basc│RCl│
+     *          │SCR│LGUI│LALT│SPC │CLIK│Ent │Basc│RAIS│RCl│
      *          └───┴────┴────┴────┴────┴────┴────┴────┴───┘
      */
-    [0] = LAYOUT(
-        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,             KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
-        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_6,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
-        KC_LCTL,  KC_A,   KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC, KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
-        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_RBRC, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
-                          SCROLL,  KC_LGUI, KC_LALT, KC_SPC,  KC_BTN1, KC_ENT,  KC_RCTL, KC_BSPC, KC_BTN2
+    [_BASE] = LAYOUT(
+        KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                      KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_6,             KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSLS,
+        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_LBRC,          KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
+        KC_LSFT, KC_NUBS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_RBRC,          KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,
+                                   SCROLL,  KC_LGUI, KC_LALT, KC_SPC,  KC_BTN1, KC_ENT,  KC_BSPC, MO(1),   KC_BTN2
+    ),
+    /*
+     *  ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
+     *  │   │F1 │F2 │F3 │F4 │F5 │       │F7 │F8 │F9 │F10│F11│F12│
+     * ┌┴───┼───┼───┼───┼───┼───┼───┐   ├───┼───┼───┼───┼───┼───┤
+     * │    │   │   │   │   │   │   │   │   │   │   │   │   │   │
+     * ├────┼───┼───┼───┼───┼───┼───┤ O ├───┼───┼───┼───┼───┼───┤
+     * │    │   │   │   │   │   │   │   │   │PGD│PGU│   │   │   │
+     * ├────┼───┼───┼───┼───┼───┼───┤   ├───┼───┼───┼───┼───┼───┤
+     * │    │   │   │   │   │   │   │   │ ← │ ↓ │ ↑ │ → │HOM│END│
+     * └────┴───┼───┼───┴┬──┴─┬─┴──┬┴───┼───┴┬──┴─┬─┴──┬┴──┬┴───┘
+     *          │   │    │    │    │    │    │    │    │   │
+     *          └───┴────┴────┴────┴────┴────┴────┴────┴───┘
+     */
+    [_RAISE] = LAYOUT(
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                     KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+        _______, _______, _______, _______, _______, _______, KC_F6,            _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,          _______, KC_PGDN, KC_PGUP, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,          KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,KC_HOME, KC_END,
+                                   _______, _______, _______, _______, _______, _______, _______, _______, _______
+    ),
+    [_LAYER_2] = LAYOUT(
+        _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
+                                   _______, _______, _______, _______, _______, _______, _______, _______, _______
+    ),
+    [_LAYER_3] = LAYOUT(
+        _______, _______, _______, _______, _______, _______,                   _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
+        _______, _______, _______, _______, _______, _______, _______,          _______, _______, _______, _______, _______, _______,
+                                   _______, _______, _______, _______, _______, _______, _______, _______, _______
     )
 };
+
+layer_state_t layer_state_set_user(layer_state_t state) {
+
+    switch (get_highest_layer(state)) {
+        case _BASE:
+            rgblight_sethsv(HSV_BLUE);
+            break;
+        case _RAISE:
+            rgblight_sethsv(HSV_CYAN);
+            break;
+        case _LAYER_2:
+            rgblight_sethsv(HSV_ORANGE);
+            break;
+        case _LAYER_3:
+            rgblight_sethsv(HSV_GREEN);
+            break;
+    }
+
+    return state;
+}
+
+layer_state_t default_layer_state_set_user(layer_state_t state) {
+    rgblight_setrgb(0x00, 0x00, 0x01);
+    return state;
+}

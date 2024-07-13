ab84def568988fc3e1a735550d5bffa623a48060
Atsushi Morimoto (74th) <site@74th.tech>
2024-07-13 21:36:53 +0900

    test TPS43 Trackpad

---
+++ keyboards/sparrowxxt/config.h
@@ -0,0 +1,11 @@
+// Copyright 2023 Atsushi Morimoto (@74th)
+// SPDX-License-Identifier: GPL-2.0-or-later
+
+#pragma once
+
+#define I2C1_SCL_PIN GP1
+#define I2C1_SDA_PIN GP0
+#define I2C_DRIVER I2CD0
+#define F_SCL 100000
+#define AZOTEQ_IQS5XX_TPS43 1
+#define AZOTEQ_IQS5XX_SCROLL_INITIAL_DISTANCE 10
---
+++ keyboards/sparrowxxt/rules.mk
@@ -0,0 +1,5 @@
+# This file intentionally left blank
+POINTING_DEVICE_ENABLE = yes
+I2C_DRIVER_REQUIRED = yes
+POINTING_DEVICE_DRIVER = azoteq_iqs5xx
+RGBLIGHT_ENABLE = yes

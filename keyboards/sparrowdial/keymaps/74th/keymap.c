119536ed4fc2eb9382e7fcfdee6ae34bd2d26f39
Atsushi Morimoto (74th) <74th.tech@gmail.com>
2025-02-22 23:07:07 +0000

    up

--- keyboards/sparrowdial/keymaps/74th/keymap.c
+++ keyboards/sparrowdial/keymaps/74th/keymap.c
@@ -135,7 +135,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,              KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
         _______,  KC_GRV,   S(KC_GRV),_______,  _______,  _______,            _______,  KC_PGDN,  KC_PGUP,  _______,  _______,  _______,
         _______,  KC_TAB,   PR_WINm,  PR_SCRm,  KC_F12,   _______,            KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_HOME,  KC_END,
-                            _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_DEL,   _______
+                            _______,  _______,  _______,  KC_BTN3,  KC_BTN2,  _______,  _______,  KC_DEL,   _______
     ),
     [_LINUX_BASE_L] = LAYOUT(
         KC_1,     KC_2,     KC_3,     KC_4,     KC_5,                         KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_BSLS,
@@ -149,7 +149,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,              KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,
         _______,  KC_GRV,   S(KC_GRV),_______,  _______,  _______,            _______,  KC_PGDN,  KC_PGUP,  _______,  _______,  _______,
         _______,  KC_TAB,   PR_WINm,  PR_SCRm,  KC_F12,   _______,            KC_LEFT,  KC_DOWN,  KC_UP,    KC_RIGHT, KC_HOME,  KC_END,
-                            _______,  _______,  _______,  _______,  _______,  _______,  _______,  KC_DEL,  _______
+                            _______,  _______,  _______,  KC_BTN3,  KC_BTN2,  _______,  _______,  KC_DEL,  _______
     ),
     [_SPECIAL_L] = LAYOUT(
         _______,  USE_MAC,  USE_LINUX,USE_DEMO, _______,                      _______,  _______,  _______,  _______,  _______,  _______,
